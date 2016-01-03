a8021349e28adbd4d81b21d5dad6b1c0502d2658
Derek <derek@skylark95.com>
2016-01-03 13:20:36 -0600

    remove links, added genre, release date, misc style changes

--- src/js/steamhover.js
+++ src/js/steamhover.js
@@ -2,32 +2,58 @@ function formatPrice(price, locale, currency) {
     var num = price / 100;
     return num.toLocaleString(locale, { style: 'currency', currency: currency });
 }
+
 function showIfTrue(selector, visibility) {
     if (visibility) {
         $(selector).show();
     }
 }
-$.get('appdetails.json').done(function(data) {
-    if (data) {
-        var appid = '730',
-            appdetails = data[appid].data,
-            locale = 'en-US',
-            price = appdetails.price_overview,
-            currency = price.currency,
-            platforms = appdetails.platforms;
 
-        $('.sh_header_image').html('<img src="' + appdetails.header_image + '"/>');
-        $('.sh_title').html(appdetails.name);
-        $('.sh_description').html(appdetails.detailed_description);
-        $('.sh_price_discount').html('(' + price.discount_percent + '%)');
-        $('.sh_price_initial').html(formatPrice(price.initial, locale, currency));
-        $('.sh_price_final').html(formatPrice(price.final, locale, currency));
+function displayAppDetails(appid) {
+    $.get('appdetails.json').done(function(data) {
+        if (data) {
+            var appdetails = data[appid].data,
+                locale = 'en-US',
+                price = appdetails.price_overview,
+                currency = price.currency,
+                platforms = appdetails.platforms;
 
-        showIfTrue('.sh_platform_win', platforms.windows);
-        showIfTrue('.sh_platform_mac', platforms.mac);
-        showIfTrue('.sh_platform_linux', platforms.linux);
+            // header
+            $('.sh_header_image').html('<img src="' + appdetails.header_image + '"/>');
 
-        $('.sh_store_page').attr('href', 'http://store.steampowered.com/app/' + appid);
-        $('.sh_steam_db').attr('href', 'https://steamdb.info/app/' + appid);
-    }
-});
+            // title
+            $('.sh_title').html(appdetails.name);
+
+            // description
+            $('.sh_description').html(appdetails.detailed_description);
+
+            // price
+            $('.sh_price_final').html(formatPrice(price.final, locale, currency));
+            if (price.discount_percent && price.discount_percent > 0) {
+                $('.sh_price_discount').html('(' + price.discount_percent + '%)');
+                $('.sh_price_initial').html(formatPrice(price.initial, locale, currency));
+            }
+
+            // platforms
+            showIfTrue('.sh_platform_win', platforms.windows);
+            showIfTrue('.sh_platform_mac', platforms.mac);
+            showIfTrue('.sh_platform_linux', platforms.linux);
+
+            // genre
+            if (appdetails.genres && appdetails.genres.length > 0) {
+                $('.sh_genre').html(appdetails.genres[0].description);
+            } else {
+                $('.sh_genre').html('Uncategorized');
+            }
+
+            // release date
+            if (appdetails.release_date && appdetails.release_date.coming_soon === false) {
+                $('.sh_release_date').html(appdetails.release_date.date);
+            } else {
+                $('.sh_release_date').html('Coming Soon');
+            }
+        }
+    });
+}
+
+displayAppDetails('730');
