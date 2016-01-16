3916c0ba0e98a0cea71b4eb423b13cffc7059c09
Derek <derek@skylark95.com>
2016-01-16 15:13:06 -0600

    promise.all

--- src/js/steamhover.js
+++ src/js/steamhover.js
@@ -40,43 +40,46 @@ function loadAppDetails(appid) {
 }
 
 function displayAppDetails(appid) {
-    loadTemplate().then(function(template) {
+    Promise.all([loadTemplate(), loadAppDetails(appid)]).then(function(values) {
+        var template = values[0],
+            data = values[1];
+
+        // template
         $('body').append(template);
-        loadAppDetails(appid).then(function(data) {
-            // header
-            $('.sh_header_image').html(data.header_image);
 
-            // title
-            $('.sh_title').html(data.title);
+        // header
+        $('.sh_header_image').html(data.header_image);
 
-            // description
-            var description = $('.sh_description');
-            description.html(data.description);
-            description.find(':header, img, ul, br, strong').remove();
-            var parts = description.text().split(' ');
-            if (parts.length > 200) {
-                parts = parts.slice(0, 200);
-                parts.push('...');
-            }
-            description.html(parts.join(' '));
-            description.show();
+        // title
+        $('.sh_title').html(data.title);
 
-            // price
-            $('.sh_price_final').html(data.price_final);
-            $('.sh_price_discount').html(data.price_discount);
-            $('.sh_price_initial').html(data.price_initial);
+        // description
+        var description = $('.sh_description');
+        description.html(data.description);
+        description.find(':header, img, ul, br, strong').remove();
+        var parts = description.text().split(' ');
+        if (parts.length > 200) {
+            parts = parts.slice(0, 200);
+            parts.push('...');
+        }
+        description.html(parts.join(' '));
+        description.show();
 
-            // platforms
-            showIfTrue('.sh_platform_win', data.platform_win);
-            showIfTrue('.sh_platform_mac', data.platform_mac);
-            showIfTrue('.sh_platform_linux', data.platform_linux);
+        // price
+        $('.sh_price_final').html(data.price_final);
+        $('.sh_price_discount').html(data.price_discount);
+        $('.sh_price_initial').html(data.price_initial);
 
-            // genre
-            $('.sh_genre').html(data.genre);
+        // platforms
+        showIfTrue('.sh_platform_win', data.platform_win);
+        showIfTrue('.sh_platform_mac', data.platform_mac);
+        showIfTrue('.sh_platform_linux', data.platform_linux);
 
-            // release date
-            $('.sh_release_date').html(data.release_date);
-        });
+        // genre
+        $('.sh_genre').html(data.genre);
+
+        // release date
+        $('.sh_release_date').html(data.release_date);
     });
 }
 
