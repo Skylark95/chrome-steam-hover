30e3d0bffdd816c6cdab668b9b4fee9a467b3c84
Derek <derek@skylark95.com>
2016-02-17 20:25:31 -0600

    gh-pages

--- install.js
+++ install.js
@@ -1,31 +1,25 @@
 function installListener() {
-    var webstoreURL = "https://chrome.google.com/webstore/detail/jfakmahmklpeigafdahkgkmnlmfjaphd";
-
-    function hideInstall() {
-        button.style.display = 'none';
-    }
-
-    function goToWebstore() {
-        window.location.href = webstoreURL;
-    }
-
-    function installFailed(errorString) {
-        console.error(errorString);
-        goToWebstore();
-    }
-
-    var button = document.getElementById('install-button'),
-        install = goToWebstore;
-    if (typeof chrome !== 'undefined') {
+    var webstoreURL = "https://chrome.google.com/webstore/detail/jfakmahmklpeigafdahkgkmnlmfjaphd",
         install = function() {
-            chrome.webstore.install(webstoreURL, hideInstall, installFailed);
+            window.location.href = webstoreURL;
         };
-        if (chrome.app.isInstalled) {
-            hideInstall();
-        }
+
+    function onSuccess() {
+        document.getElementById('install-button').disabled = true;
+        document.getElementById('install-button-text').innerHTML = 'Installed';
     }
-    
-    button.addEventListener('click', install);
+
+    function onFailure(errorString) {
+        console.error(errorString);
+    }
+
+    if (typeof chrome !== 'undefined' && bowser.chrome) {
+        install = function() {
+            chrome.webstore.install(webstoreURL, onSuccess, onFailure);
+        };
+    }
+
+    document.getElementById('install-button').addEventListener('click', install);
 }
 
 installListener();
