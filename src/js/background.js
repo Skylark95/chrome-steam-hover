7d52e860f28002384cff7caf03d3075550598903
Derek <derek@skylark95.com>
2016-04-01 20:34:22 -0500

    refactor

--- src/js/background.js
+++ src/js/background.js
@@ -111,80 +111,103 @@ function appdetailsListener(request, sender, sendResponse) {
 
                 response.success = appResponse.success && appResponse.data[appid].success;
                 if (response.success) {
-                    var appdetails = appResponse.data[appid].data,
-                        locale = 'en-US',
-                        platforms = appdetails.platforms;
-
-                    // header
-                    response.data.header_image = '<img src="' + appdetails.header_image + '"/>';
-
-                    // title
-                    response.data.title = appdetails.name;
-
-                    // description
-                    response.data.description = appdetails.about_the_game;
-
-                    // price
-                    response.data.price_discount = '';
-                    response.data.price_initial = '';
-                    if (appdetails.is_free) {
-                        response.data.price_final = 'FREE';
-                    } else {
-                        var price = appdetails.price_overview,
-                            currency = price.currency;
-                        response.data.price_final = formatPrice(price.final, locale, currency);
-                        if (price.discount_percent && price.discount_percent > 0) {
-                            response.data.price_discount = '(' + price.discount_percent + '%)';
-                            response.data.price_initial = formatPrice(price.initial, locale, currency);
-                        }
-                    }
-
-                    // platforms
-                    response.data.platform_win = platforms.windows;
-                    response.data.platform_mac = platforms.mac;
-                    response.data.platform_linux = platforms.linux;
-
-                    // genre
-                    if (appdetails.genres && appdetails.genres.length > 0) {
-                        response.data.genre = appdetails.genres[0].description;
-                    } else {
-                        response.data.genre = 'Uncategorized';
-                    }
-
-                    // release date
-                    if (appdetails.release_date && appdetails.release_date.coming_soon === false) {
-                        response.data.release_date = appdetails.release_date.date;
-                    } else {
-                        response.data.release_date = 'Coming Soon';
-                    }
-
-                    // trading cards
-                    response.data.trading_cards = hasCategory(TRADING_CARDS_ID, appdetails.categories);
-
-                    // ownership
-                    response.data.user_signed_in = false;
-                    response.data.too_many_requests = false;
-                    if (userResponse.status && userResponse.status === 429) {
-                        response.data.too_many_requests = true;
-                    } else if (userResponse.success) {
-                        var appuserdetails = userResponse.data[appid].success && userResponse.data[appid].data;
-                        if (appuserdetails) {
-                            response.data.user_signed_in = true;
-                            response.data.is_owned = appuserdetails.is_owned;
-                            response.data.added_to_wishlist = appuserdetails.added_to_wishlist;
-                        }
-                    }
+                    var appdetails = appResponse.data[appid].data;
+                    setHeaderImage(response, appdetails);
+                    setTitle(response, appdetails);
+                    setDescription(response, appdetails);
+                    setPrice(response, appdetails, 'en-US');
+                    setPlatforms(response, appdetails);
+                    setGenre(response, appdetails);
+                    setReleaseDate(response, appdetails);
+                    setTradingCards(response, appdetails);
+                    setOwnership(response, appdetails, appid, userResponse);
 
                     // Send response
                     sendResponse(response);
                 }
             });
         });
-
         return true;
     }
 }
 
+/*
+ * Build response functions
+ */
+function setHeaderImage(response, appdetails) {
+    response.data.header_image = '<img src="' + appdetails.header_image + '"/>';
+}
+
+function setTitle(response, appdetails) {
+    response.data.title = appdetails.name;
+}
+
+function setDescription(response, appdetails) {
+    response.data.description = appdetails.about_the_game;
+}
+
+function setPrice(response, appdetails, locale) {
+    response.data.price_discount = '';
+    response.data.price_initial = '';
+    if (appdetails.is_free) {
+        response.data.price_final = 'FREE';
+    } else {
+        var price = appdetails.price_overview,
+            currency = price.currency;
+        response.data.price_final = formatPrice(price.final, locale, currency);
+        if (price.discount_percent && price.discount_percent > 0) {
+            response.data.price_discount = '(' + price.discount_percent + '%)';
+            response.data.price_initial = formatPrice(price.initial, locale, currency);
+        }
+    }
+}
+
+function setPlatforms(response, appdetails) {
+    var platforms = appdetails.platforms;
+    response.data.platform_win = platforms.windows;
+    response.data.platform_mac = platforms.mac;
+    response.data.platform_linux = platforms.linux;
+}
+
+function setGenre(response, appdetails) {
+    if (appdetails.genres && appdetails.genres.length > 0) {
+        response.data.genre = appdetails.genres[0].description;
+    } else {
+        response.data.genre = 'Uncategorized';
+    }
+}
+
+function setReleaseDate(response, appdetails) {
+    if (appdetails.release_date && appdetails.release_date.coming_soon === false) {
+        response.data.release_date = appdetails.release_date.date;
+    } else {
+        response.data.release_date = 'Coming Soon';
+    }
+}
+
+function setTradingCards(response, appdetails) {
+    response.data.trading_cards = hasCategory(TRADING_CARDS_ID, appdetails.categories);
+}
+
+function setOwnership(response, appdetails, appid, userResponse) {
+    response.data.user_signed_in = false;
+    response.data.too_many_requests = false;
+    if (userResponse.status && userResponse.status === 429) {
+        response.data.too_many_requests = true;
+    } else if (userResponse.success) {
+        var appuserdetails = userResponse.data[appid].success && userResponse.data[appid].data;
+        if (appuserdetails) {
+            response.data.user_signed_in = true;
+            response.data.is_owned = appuserdetails.is_owned;
+            response.data.added_to_wishlist = appuserdetails.added_to_wishlist;
+        }
+    }
+}
+
+
+/*
+ * Add listeners
+ */
 chrome.runtime.onMessage.addListener(templateListener);
 chrome.runtime.onMessage.addListener(appdetailsListener);
 chrome.runtime.onMessage.addListener(loadoptionsListener);
--- src/js/steamhover.js
+++ src/js/steamhover.js
@@ -1,12 +1,9 @@
 var steamAppPage = new RegExp("^https?://store.steampowered.com/app/([0-9]+)/?.*$"),
     tooltipDelay = 200;
 
-function showIfTrue(hoverbox, selector, visibility) {
-    if (visibility) {
-        hoverbox.find(selector).css('display', 'inline-block');
-    }
-}
-
+/*
+ * Event Listener
+ */
 function hoverEventListener() {
     var matches = this.href.match(steamAppPage);
     if (matches && matches.length > 1) {
@@ -29,6 +26,9 @@ function hoverEventListener() {
     }
 }
 
+/*
+ * Background Page
+ */
 function loadTemplate() {
     return new Promise(function(resolve, reject) {
         chrome.runtime.sendMessage({operation: 'loadtemplate'}, function(response) {
@@ -57,6 +57,9 @@ function loadAppDetails(appid) {
     });
 }
 
+/*
+ * Display
+ */
 function displayAppDetails(appid) {
     return new Promise(function(resolve, reject) {
         Promise.all([loadTemplate(), loadAppDetails(appid)]).then(function(values) {
@@ -65,65 +68,16 @@ function displayAppDetails(appid) {
                 options = values[1].options,
                 hoverbox = $('<div class="sh_app"></div>');
 
-            // appid
-            hoverbox.attr('sh_appid', appid);
-
-            // template
-            hoverbox.html(template);
-
-            // header
-            hoverbox.find('.sh_header_image').html(data.header_image);
-
-            // title
-            var title = hoverbox.find('.sh_title a');
-            title.text(data.title);
-            title.attr('href', 'http://store.steampowered.com/app/' + appid);
-
-            // title too many requests
-            if (data.too_many_requests) {
-                title.attr('title', 'Too Many Requests');
-            }
-
-            // title user sign in
-            if (data.user_signed_in) {
-                title.attr('title', 'You are signed into Steam');
-            }
-
-            // title on wishlist
-            if (data.added_to_wishlist) {
-                title.attr('style', 'color: ' + options.wishlist_color + ' !important');
-                title.attr('title', 'On Wishlist');
-            }
-
-            // title owned
-            if (data.is_owned) {
-                title.attr('style', 'color: ' + options.owned_color + ' !important');
-                title.attr('title', 'You already own this game');
-            }
-
-            // description
-            var description = hoverbox.find('.sh_description');
-            description.html(data.description);
-            formatDescription(description);
-
-            // price
-            hoverbox.find('.sh_price_final').html(data.price_final);
-            hoverbox.find('.sh_price_discount').html(data.price_discount);
-            hoverbox.find('.sh_price_initial').html(data.price_initial);
-
-            // platforms
-            showIfTrue(hoverbox, '.sh_platform_win', data.platform_win);
-            showIfTrue(hoverbox, '.sh_platform_mac', data.platform_mac);
-            showIfTrue(hoverbox, '.sh_platform_linux', data.platform_linux);
-
-            // trading cards
-            showIfTrue(hoverbox, '.sh_trading_cards', data.trading_cards);
-
-            // genre
-            hoverbox.find('.sh_genre').html(data.genre);
-
-            // release date
-            hoverbox.find('.sh_release_date').html(data.release_date);
+            setAppId(hoverbox, appid);
+            setTemplate(hoverbox, template);
+            setHeaderImage(hoverbox, data);
+            setTitle(hoverbox, data, appid, options);
+            setDescription(hoverbox, data);
+            setPrice(hoverbox, data);
+            setPlatforms(hoverbox, data);
+            setTradingCards(hoverbox, data);
+            setGenere(hoverbox, data);
+            setReleaseDate(hoverbox, data);
 
             // mouse wheel event
             hoverbox.on('wheel', handleWheelEvent);
@@ -135,14 +89,83 @@ function displayAppDetails(appid) {
     });
 }
 
-function handleWheelEvent(event) {
-    if (event.target.className !== 'sh_description') {
-        var description = $(this).find('.sh_description')[0];
-        description.scrollTop += event.originalEvent.deltaY;
-        event.preventDefault();
+/*
+ * Build hoverbox
+ */
+function setAppId(hoverbox, appid) {
+    hoverbox.attr('sh_appid', appid);
+}
+
+function setTemplate(hoverbox, template) {
+    hoverbox.html(template);
+}
+
+function setHeaderImage(hoverbox, data) {
+    hoverbox.find('.sh_header_image').html(data.header_image);
+}
+
+function setTitle(hoverbox, data, appid, options) {
+    // title
+    var title = hoverbox.find('.sh_title a');
+    title.text(data.title);
+    title.attr('href', 'http://store.steampowered.com/app/' + appid);
+
+    // title too many requests
+    if (data.too_many_requests) {
+        title.attr('title', 'Too Many Requests');
+    }
+
+    // title user sign in
+    if (data.user_signed_in) {
+        title.attr('title', 'You are signed into Steam');
+    }
+
+    // title on wishlist
+    if (data.added_to_wishlist) {
+        title.attr('style', 'color: ' + options.wishlist_color + ' !important');
+        title.attr('title', 'On Wishlist');
+    }
+
+    // title owned
+    if (data.is_owned) {
+        title.attr('style', 'color: ' + options.owned_color + ' !important');
+        title.attr('title', 'You already own this game');
     }
 }
 
+function setDescription(hoverbox, data) {
+    var description = hoverbox.find('.sh_description');
+    description.html(data.description);
+    formatDescription(description);
+}
+
+function setPrice(hoverbox, data) {
+    hoverbox.find('.sh_price_final').html(data.price_final);
+    hoverbox.find('.sh_price_discount').html(data.price_discount);
+    hoverbox.find('.sh_price_initial').html(data.price_initial);
+}
+
+function setPlatforms(hoverbox, data) {
+    showIfTrue(hoverbox, '.sh_platform_win', data.platform_win);
+    showIfTrue(hoverbox, '.sh_platform_mac', data.platform_mac);
+    showIfTrue(hoverbox, '.sh_platform_linux', data.platform_linux);
+}
+
+function setTradingCards(hoverbox, data) {
+    showIfTrue(hoverbox, '.sh_trading_cards', data.trading_cards);
+}
+
+function setGenere(hoverbox, data) {
+    hoverbox.find('.sh_genre').html(data.genre);
+}
+
+function setReleaseDate(hoverbox, data) {
+    hoverbox.find('.sh_release_date').html(data.release_date);
+}
+
+/*
+ * Misc functions
+ */
 function formatDescription(description) {
     // Remove Images and surrounding line breaks
     description.find('img').each(function() {
@@ -162,6 +185,23 @@ function formatDescription(description) {
     });
 }
 
+function showIfTrue(hoverbox, selector, visibility) {
+    if (visibility) {
+        hoverbox.find(selector).css('display', 'inline-block');
+    }
+}
+
+function handleWheelEvent(event) {
+    if (event.target.className !== 'sh_description') {
+        var description = $(this).find('.sh_description')[0];
+        description.scrollTop += event.originalEvent.deltaY;
+        event.preventDefault();
+    }
+}
+
+/*
+ * Run It
+ */
 function steamhover() {
     if (window.location.host !== 'store.steampowered.com') {
         // Load Options
